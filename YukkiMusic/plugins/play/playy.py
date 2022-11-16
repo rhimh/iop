744899396c778fd8303085298abe3c628e9fadca
babloo-musiC <117233028+babloo-musiC@users.noreply.github.com>
2022-11-16 18:46:54 +0200

    Update playy.py

--- YukkiMusic/plugins/play/playy.py
+++ YukkiMusic/plugins/play/playy.py
@@ -36,7 +36,24 @@ from YukkiMusic.utils.inline.play import (livestream_markup,
 from YukkiMusic.utils.inline.playlist import botplaylist_markup
 from YukkiMusic.utils.logger import play_logs
 from YukkiMusic.utils.stream.stream import stream
-      
+ 
+  
+  force_btn = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(   
+              text=f"{YAFA_NAME}", url=f"{YAFA_CHANNEL}",)                        
+        ],        
+    ]
+)
+async def check_is_joined(message):    
+    try:
+        userid = message.from_user.id
+        status = await app.get_chat_member(f"{CHANNEL_SUDO}", userid)
+        return True
+    except Exception:
+        await message.reply_text("**⚠️︙عذراً، عليك الانضمام الى قناة البوت أولاً :**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+        return False
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
