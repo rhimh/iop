540d34a495c26f4fd1fda3bd4e0dccc9ec6b4a57
babloo-musiC <117233028+babloo-musiC@users.noreply.github.com>
2022-11-16 19:00:05 +0200

    Update playy.py

--- YukkiMusic/plugins/play/playy.py
+++ YukkiMusic/plugins/play/playy.py
@@ -41,7 +41,7 @@ from YukkiMusic.utils.stream.stream import stream
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 @app.on_message(
-    command(["تشغيل","قناه","قناة"])
+    command(["تشغيل","قناه","قناة","/play","شغل"])
     & filters.channel
     & ~filters.edited
     & ~BANNED_USERS
