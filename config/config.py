a5c00e8ccdebaacc4b8046adc11d62e6d193d467
hhhhhplm <120011061+hhhhhplm@users.noreply.github.com>
2022-12-07 11:03:11 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/babloo-musiC/-",
+    "https://github.com/hhhhhplm/music",
 )
 BOT_USERNAME = getenv(
     "BOT_USERNAME", "LAVAXBOT"
